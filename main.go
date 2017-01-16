68ab29faddcbc5cd352e22b400d952d6debe47ae
Andrey Chausenko <info@memz.co>
2017-01-16 16:36:08 +1100

    Implemented docker container monitoring

--- main.go
+++ main.go
@@ -3,23 +3,108 @@ package main
 import (
 	"github.com/fsouza/go-dockerclient"
 	"fmt"
+	"flag"
 )
 
-func main()  {
+type TrackedEventType int
 
-	endpoint := "unix:///var/run/docker.sock"
+const (
+	UNKNOWN_EVENT TrackedEventType = iota
+	POSITIVE_EVENT
+	NEGATIVE_EVENT
+)
+
+const endpoint = "unix:///var/run/docker.sock"
+
+var falseStrings map[string]bool
+var trackedEvents map[string]TrackedEventType
+var containerById map[string]*docker.APIContainers
+var filterLabel *string
+
+func main() {
+	filterLabel = flag.String("label", "monitor-logs", "Look for this label in docker containers")
+	flag.Parse()
+	fmt.Printf("Looking for containers labeled with: %s\n", *filterLabel)
+
+	falseStrings = map[string]bool{
+		"0": true,
+		"null": true,
+		"false": true,
+		"disable": true,
+		"disabled": true,
+		"": true,
+	}
+
+	trackedEvents = map[string]TrackedEventType{
+		"create": POSITIVE_EVENT,
+		"restart": POSITIVE_EVENT,
+		"start": POSITIVE_EVENT,
+		"destroy": NEGATIVE_EVENT,
+		"die": NEGATIVE_EVENT,
+		"kill": NEGATIVE_EVENT,
+		"stop": NEGATIVE_EVENT,
+	}
+
 	client, err := docker.NewClient(endpoint)
 	if err != nil {
 		panic(err)
 	}
 
+	containerById = make(map[string]*docker.APIContainers)
+
+	containers, err := client.ListContainers(docker.ListContainersOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		if !falseStrings[container.Labels[*filterLabel]] {
+			updateContainer(&container)
+		}
+	}
+
+	// listen for events
 	eventListener := make(chan *docker.APIEvents)
 	client.AddEventListener(eventListener)
 
 	for {
 		event := <-eventListener
-		fmt.Printf("ID=%s, Status=%s, Type=%s, Action=%s, From=%s, Actor=%s", event.ID, event.Status, event.Type, event.Action, event.From, event.Actor)
+		trackedEvent := trackedEvents[event.Status]
+		if trackedEvent == POSITIVE_EVENT {
+			if container := containerById[event.ID]; container == nil {
+				// if new container
+				if !falseStrings[container.Labels[*filterLabel]] {
+					containers, err := client.ListContainers(docker.ListContainersOptions{})
+					if err != nil {
+						panic(err)
+					}
+
+					for _, container := range containers {
+						if container.ID == event.ID {
+							updateContainer(&container)
+							break
+						}
+					}
+				}
+			}
+		} else if trackedEvent == NEGATIVE_EVENT {
+			container := containerById[event.ID]
+			if container != nil {
+				removeContainer(container)
+			}
+		}
 	}
 
 	fmt.Println("Terminating the service")
 }
+
+func updateContainer(container *docker.APIContainers) {
+	fmt.Printf("Container Up: ID=%s Labels=%s Image=%s Names=%s\n", container.ID, container.Labels, container.Image, container.Names)
+	containerById[container.ID] = container
+}
+
+func removeContainer(container *docker.APIContainers) {
+	fmt.Printf("Container Down: ID=%s Labels=%s Image=%s Names=%s\n", container.ID, container.Labels, container.Image, container.Names)
+	delete(containerById, container.ID)
+}
+
