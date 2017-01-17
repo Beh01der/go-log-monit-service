238bdea796caf693179afbdb472d93775794fcad
Andrey Chausenko <info@memz.co>
2017-01-17 12:54:22 +1100

    Migrated t a different statsd client implementation

--- main.go
+++ main.go
@@ -5,9 +5,10 @@ import (
 	"flag"
 	"github.com/fsouza/go-dockerclient"
 	"github.com/vjeantet/grok"
-	"github.com/cactus/go-statsd-client/statsd"
 	"strings"
 	"strconv"
+	"github.com/quipo/statsd"
+	"time"
 )
 
 const logPattern = `%{IP:client} \[%{TIMESTAMP_ISO8601:timestamp}\] "%{WORD:method} %{URIHOST:site}%{URIPATHPARAM:url}" %{INT:code} %{INT:request} %{INT:response} - %{NUMBER:took} \[%{DATA:cache}\] "%{DATA:mtag}" "%{DATA:agent}"`
@@ -20,31 +21,36 @@ func main() {
 	fmt.Printf("Looking for containers labeled with: %s\n", *filterLabel)
 	fmt.Printf("Using StatsD on: %s\n", *statsdAddr)
 
-	sdc, err := statsd.NewClient(*statsdAddr, "statsd-client")
+	sdc := statsd.NewStatsdClient(*statsdAddr, "")
+	err := sdc.CreateSocket()
 	if err != nil {
 		panic(err)
 	}
+	stats := statsd.NewStatsdBuffer(time.Second * 2, sdc)
 	defer sdc.Close()
 
 	g, _ := grok.New()
 
 	StartDockerLogMonitor(DockerLogMonitorConfig{FilterLabel:*filterLabel, Handler:func(container *docker.APIContainers, logEntry *LogEntry) {
-		values, _ := g.Parse(logPattern, logEntry.Log)
+		values, err := g.Parse(logPattern, logEntry.Log)
+		if err != nil {
+			return
+		}
 
 		code, _ := values["code"]
 		url, _ := values["url"]
 
-		if codeInt, _ := strconv.Atoi(code); codeInt > 99 {
-			sdc.Inc("router.hit", 1, 1.0)
-			sdc.Inc("router.hit." + code, 1, 1.0)
+		if codeInt, err := strconv.Atoi(code); err == nil && codeInt > 99 {
+			stats.Incr("router.hit", 1)
+			stats.Incr("router.hit." + code, 1)
 		}
 
 		if url != "" {
 			if strings.Contains(url, "api/note") {
-				sdc.Inc("api.note.hit", 1, 1.0)
-				sdc.Inc("api.note", 1, 1.0)
+				stats.Incr("api.note.hit", 1)
+				stats.Incr("api.note", 1)
 			} else if strings.Contains(url, "api/policy") {
-				sdc.Inc("api.hit", 1, 1.0)
+				stats.Incr("api.hit", 1)
 			}
 		}
 	}})
